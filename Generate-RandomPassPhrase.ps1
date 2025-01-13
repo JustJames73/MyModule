9423c4a782d7e129f94f504d9020bfc436965a1c
James <JustJames73@users.noreply.github.com>
2025-01-13 15:28:40 -0700

    Update Generate-RandomPassPhrase.ps1
    Added comments

--- Generate-RandomPassPhrase.ps1
+++ Generate-RandomPassPhrase.ps1
@@ -46,43 +46,70 @@ This function may require access to external word list files. Ensure appropriate
         [array]$BannedWords = @('1234','2015','2016','2017','2018','2019','2020','2021','2022','2023','avalanche','broncos','buffalo','buffs','colorado','horse','mammoth','nuggets','rams','rockies','battery','drive','energy','nrel','research','solar','wind','correct','password','qwerty','rain')
     )
     
+    #Use a blank space as the character between words, this is modified elsewhere of the Complex switch is used
     $PadCharacters = ' '
+    
+    # Initialize an array to store passphrases
     $passphrases = @()
 
+    # Check if the word list file exists
     if (-not (Test-Path -Path $wordListFile -PathType Leaf)) {
         Write-Verbose -Message "Word list file '$wordListFile' not found. Searching for .words files in the current directory..."
+
+        # Search for *.word files in the current directory
         $wordListFiles = Get-ChildItem -Path $pwd -Filter "*.words" -File -Recurse
         Write-Verbose -Message "Found the following words list: $wordListFiles"
         
+        # If no *.words files found, throw an error
         if ($wordListFiles.Count -eq 0) {
             throw "No .words files found in the current directory."
         }
+
+        # Use the first found .word file
         $wordListFile = $wordListFiles[0].FullName
     }
 
+    # Create the wordList as a global variable so that it is not processed everytime the function is run
     if (-not $wordlist) {
         $global:wordList = Get-Content -Path $wordListFile 
     }
 
+#region - PassPhrase generation
+    # Generate passphrases for each iteration
     for ($i = 1; $i -le $Iterations; $i++) {
         $password = ''
         $length = 0
 
+        # Keep adding random words until the password length is at least $minLength
         while ($length -lt $minLength) {
+            
+            # Get a random word from the list
             $randomWord = Get-Random -InputObject $global:wordList
 
+	        # Check if the $radomWord matches $BannedWord
             foreach ($bannedWord in $BannedWords) {
                 $isSafe = $true
+
+                # Use case-insensitive regular expression match
                 if ($randomWord -match [regex]::Escape($bannedWord) -and $matches[0]) {
                     $isSafe = $false
                     break
                 }
             }
 
+	        # If the word is safe, add it to $password
             if ($isSafe) {
+
+                # if the Complex switch is used, modify the case
                 if ($Complex) {
+
+                    # Special characters to inject, every other char is a space; 50% chance of giving a space. 
                     $PadCharacters = @(' ', ',', ' ', '.', ' ', '/', ' ', '\', ' ', ';', ' ', '-')
+
+                    # Generate a random number to determine the case
                     $case = Get-Random -Minimum 0 -Maximum 4
+
+                    # Convert the word to the selected case
                     switch ($case) {
                         0 { $password += $randomWord.ToLower() }
                         1 { $password += $randomWord }
@@ -91,22 +118,33 @@ This function may require access to external word list files. Ensure appropriate
                     }
                 }
                 else {
+                    #Add the unmodified word to the password string
                     $password += $randomWord
                 }
             }
 
+            # Check the length and continue processing of too short
             $length = $password.Length
             if ($length -lt $minLength) {
+
+                # Add a special character, the character set is modified when the Complex switch is used
                 $PadChar = Get-Random -InputObject $PadCharacters
                 $password += $PadChar
+
+                # Increment length to account for the added space
                 $length++
             }
         }
 
+        # Remove trailing space
         $password = $password.TrimEnd()
+
+        # Add the passphrase to the array
         $passphrases += $password
     }
+#region - PassPhrase generation
 
+    #Output the password, multiple passwords when the Iterations paramater is used
     $passphrases
 }
 
